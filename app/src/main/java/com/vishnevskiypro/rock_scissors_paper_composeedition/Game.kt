d3f6027e4ff1591ffff89e6f9abea1650a806f64
Oleg <vishnevskiyoleg@gmail.com>
2022-04-05 02:34:36 +0300

    Game logic were added

--- app/src/main/java/com/vishnevskiypro/rock_scissors_paper_composeedition/Game.kt
+++ app/src/main/java/com/vishnevskiypro/rock_scissors_paper_composeedition/Game.kt
@@ -1,2 +1,16 @@
 package com.vishnevskiypro.rock_scissors_paper_composeedition
 
+fun whoWin (human: String, computer: String): String {
+    var result = ""
+    if (human == computer){
+        result = "Ничья !"
+    } else if ((computer == "Rock" && human == "Paper")
+        || (computer == "Paper" && human == "Scissors")
+        || (computer == "Scissors" && human == "Rock")){
+        result = "you WIN !!!"
+    } else {
+        result = "you lose (("
+    }
+
+        return "Human is $human, Computer is $computer, $result"
+}
--- app/src/main/java/com/vishnevskiypro/rock_scissors_paper_composeedition/MainActivity.kt
+++ app/src/main/java/com/vishnevskiypro/rock_scissors_paper_composeedition/MainActivity.kt
@@ -1,8 +1,5 @@
 package com.vishnevskiypro.rock_scissors_paper_composeedition
 
-
-
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,10 +13,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
 
 
 class MainActivity : ComponentActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,6 +29,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting (){
+    val options = arrayOf("Rock", "Scissors", "Paper")
+
+    val humanValue = remember {
+        mutableStateOf("Human")
+    }
+    val computerValue = remember {
+        mutableStateOf("Computer")
+    }
+    val gameResult = remember {
+        mutableStateOf("Chose to Play")
+    }
+
+
     Column(horizontalAlignment = Alignment.CenterHorizontally,modifier = Modifier
         .fillMaxWidth()
         .padding(vertical = 100.dp)
@@ -37,29 +49,37 @@ fun Greeting (){
     ) {
         Text(text = "Rock, Scissors, Paper!", fontSize = 32.sp)
 
-        Text(text = "Chose to Play!", fontSize = 44.sp, modifier = Modifier
+        Text(text = gameResult.value, fontSize = 30.sp, modifier = Modifier
             .padding(vertical = 12.dp))
 
-        OutlinedTextField(value = "Human", onValueChange = {}, modifier = Modifier
+        OutlinedTextField(value = humanValue.value, onValueChange = {}, modifier = Modifier
             .padding(top = 45.dp))
 
-        OutlinedTextField(value = "Computer", onValueChange = {}, modifier = Modifier
+        OutlinedTextField(value = computerValue.value, onValueChange = {}, modifier = Modifier
             .padding(top = 8.dp))
 
         Row(modifier = Modifier.padding(vertical = 25.dp)) {
-            OutlinedButton(onClick = { /*TODO*/ }) {
+            OutlinedButton(onClick = {humanValue.value = "Rock"}) {
                 Text(text = "Rock")
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
+            OutlinedButton(onClick = {humanValue.value = "Scissors"}) {
                 Text(text = "Scissors")
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
+            OutlinedButton(onClick = {humanValue.value = "Paper"}) {
                 Text(text = "Paper ")
             }
 
         }
         
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = {
+            computerValue.value = options[(Math.random()*options.size).toInt()]
+
+            val humanParam: String = humanValue.value
+            val computerParam: String = computerValue.value
+            gameResult.value = whoWin(humanParam, computerParam)
+
+
+        }, modifier = Modifier.padding(top = 20.dp)) {
             Text(text = "Play")
         }
 
